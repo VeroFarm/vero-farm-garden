2a963b739203923a064862ab301a076b6d99daf7
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-10-03 21:54:15 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,15 +1,78 @@
 {
-  "vero_intro_sound": true,
-  "collections": {
+  "vero_intro_sound": false,
+  "collectionsVero": {
     "attribute": [
-      
+      {
+        "index": '0',
+        "name": 'Normal',
+        "cover": '/images2/attributes/normal.png',
+      },
+      { 
+        "index": '1',
+        "name: 'Advance',
+        "cover": '/images2/attributes/advance.png',
+      },
+      {
+        index: '2',
+        name: 'Legend',
+        cover: '/images2/attributes/legend.png',
+      },
     ],
-    "level": {
-      "level0": {
-        "name": {
-          "cover": ""
-        }
-      }
-    }
+    "levels": [
+      {
+        AER: {
+          name: 'Kano',
+          cover: '/images2/veros/lv0/0.gif',
+        },
+        TERRA: {
+          name: 'Ono',
+          cover: '/images2/veros/lv0/1.gif',
+        },
+        IGNIS: {
+          name: 'Taka',
+          cover: '/images2/veros/lv0/2.gif',
+        },
+        AQUA: {
+          name: 'Sake',
+          cover: '/images2/veros/lv0/3.gif',
+        },
+      },
+      {
+        AER: {
+          name: 'Sunflower angel',
+          cover: '/images2/veros/lv1/0.gif',
+        },
+        TERRA: {
+          name: 'Garlic angel',
+          cover: '/images2/veros/lv1/1.gif',
+        },
+        IGNIS: {
+          name: 'Cactus angel',
+          cover: '/images2/veros/lv1/2.gif',
+        },
+        AQUA: {
+          name: 'Cabbage angel',
+          cover: '/images2/veros/lv1/3.gif',
+        },
+      },
+      {
+        AER: {
+          name: 'Sunflower angel',
+          cover: '/images2/veros/lv2/0.gif',
+        },
+        TERRA: {
+          name: 'Garlic angel',
+          cover: '/images2/veros/lv2/1.gif',
+        },
+        IGNIS: {
+          name: 'Cactus angel',
+          cover: '/images2/veros/lv2/2.gif',
+        },
+        AQUA: {
+          name: 'Cabbage angel',
+          cover: '/images2/veros/lv2/3.gif',
+        },
+      },
+    ],
   }
 }
