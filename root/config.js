e203768f27c4120b87a81263e4e94400c9ae28d3
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-10-03 20:33:37 +0700

    Create config.js

---
+++ root/config.js
@@ -0,0 +1,15 @@
+{
+  "vero_intro_sound": true,
+  "collections": {
+    "attribute": [
+      
+    ],
+    "level": {
+      "level0": {
+        "name": {
+          "cover": ""
+        }
+      }
+    }
+  }
+}
